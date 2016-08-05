66f3104c61dafe376e7af15dc402896842797e97
haridu <harindu96@gmail.com>
2016-08-06 10:51:55 +1200

    Add files via upload

---
+++ functions.ts
@@ -0,0 +1,16 @@
+function myFunction() {
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+
+        cell1.innerHTML = 0;
+        cell2.innerHTML = document.getElementById("inputitemname").value;
+        cell3.innerHTML = document.getElementById("inputcity").value;
+        cell4.innerHTML = document.getElementById("inputFoundLoactione").value;
+        cell5.innerHTML = document.getElementById("inputFounderemail").value;
+    }
+   
