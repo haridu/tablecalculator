eb5a14e5cc3a62224e0174bf5910f4ac41418031
haridu <harindu96@gmail.com>
2016-09-03 15:30:28 +1200

    Update indexfun.ts

--- indexfun.ts
+++ indexfun.ts
@@ -30,8 +30,12 @@ function formula(){
    var Number_of_rows_in_block= Math.floor(Availspace/rowspace);
    
    var total_blocks_req=rowsrequired/Number_of_rows_in_block;
-   var tablesize_in_bytes=total_blocks_req*block_size;
+   var tablesize_in_bytes :number=total_blocks_req*block_size;
    
-    alert(tablesize_in_bytes);
-	
+   
+   if(!(KCBH_IN>0||UB4>0||KTBBH>0||INITRANS>0||KTBIT>0||KDBH>0)){
+	alert("please fill all feilds.for more information press infor button");
+   }else{
+    alert("The table is "+tablesize_in_bytes+" bytes");
+   }
  }
