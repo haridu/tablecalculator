bb3d6280a927133fd36ccf62563e88d8d3eba09a
haridu <harindu96@gmail.com>
2016-09-03 15:28:44 +1200

    Update indexfun.js

--- indexfun.js
+++ indexfun.js
@@ -21,11 +21,10 @@ function formula() {
     var Number_of_rows_in_block = Math.floor(Availspace / rowspace);
     var total_blocks_req = rowsrequired / Number_of_rows_in_block;
     var tablesize_in_bytes = total_blocks_req * block_size;
-    if (KCBH_IN == 0 || UB4 == 0 || KTBBH == 0 || INITRANS == 0 || KTBIT == 0 || KDBH == 0) {
+    if (!(KCBH_IN > 0 || UB4 > 0 || KTBBH > 0 || INITRANS > 0 || KTBIT > 0 || KDBH > 0)) {
         alert("please fill all feilds.for more information press infor button");
     }
     else {
         alert("The table is " + tablesize_in_bytes + " bytes");
     }
 }
-
